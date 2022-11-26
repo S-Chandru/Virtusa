6e97f8efd4c4e58400acaabca33bbd925bf79266
S-Chandru <113829052+S-Chandru@users.noreply.github.com>
2022-11-26 10:31:53 +0530

    Add files via upload

---
+++ C#/ASCII.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int asciiValue;
+        int i = 0;
+        for (asciiValue = 33; asciiValue <= 127; asciiValue++)
+        {
+            if ((i % 5) == 0)
+            {
+                Console.Write("\n");
+            }
+            Console.Write(" " + Convert.ToChar(asciiValue));
+            i++;
+        }
+    }
+}
